abe35f67244ef1f3a34d52ccda77d5253284f9eb
Jasper <tobias.jasper@outlook.de>
2023-07-24 01:02:31 +0200

    - Upgrade android sdk to version 34 and Java version to 17

--- app/src/main/java/school/androidgame/databaseContext/HighscoreDatabaseContext.java
+++ app/src/main/java/school/androidgame/databaseContext/HighscoreDatabaseContext.java
@@ -1,16 +1,9 @@
 package school.androidgame.databaseContext;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.Date;
-
-import school.androidgame.Highscore;
+import androidx.annotation.Nullable;
 
 public class HighscoreDatabaseContext extends SQLiteOpenHelper {
 
@@ -22,9 +15,6 @@ public class HighscoreDatabaseContext extends SQLiteOpenHelper {
                     + " points integer not null, "
                     + " creation_date Timestamp not null, "
                     + " time_played integer not null)";
-    private static final String SELECT_HIGHSCORE_LIST = "SELECT * FROM "
-                    + HighscoreDatabaseContext.TABLE_NAME
-                    + " ORDER BY points DESC LIMIT 5;";
 
 
     // Database Version
@@ -48,40 +38,5 @@ public class HighscoreDatabaseContext extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + HighscoreDatabaseContext.TABLE_NAME);
         // Create tables again
         onCreate(db);
-    }
-
-    public void insertHighscore(Highscore highScore) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put("points", highScore.getScore());
-        values.put("creation_date", highScore.getTimestamp().getTime());
-        values.put("time_played", highScore.getRuntime());
-
-        db.insert(HighscoreDatabaseContext.TABLE_NAME, null, values);
-    }
-
-    public ArrayList<Highscore> selectHighscores(int amount) {
-        ArrayList<Highscore> highscores = new ArrayList<Highscore>();
-        SQLiteDatabase db = this.getWritableDatabase();
-        String selectQuery = "SELECT  * FROM " + HighscoreDatabaseContext.TABLE_NAME + " ORDER BY " +
-                "points DESC LIMIT " + amount;
-
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        if (cursor.moveToFirst()) {
-            do {
-                Highscore highscore = new Highscore(
-                        cursor.getInt(cursor.getColumnIndex("points")),
-                        cursor.getInt(cursor.getColumnIndex("time_played")),
-                        new Date(cursor.getInt(cursor.getColumnIndex("creation_date")))
-                );
-                highscores.add(highscore);
-            } while (cursor.moveToNext());
-        }
-
-        cursor.close();
-        db.close();
-
-        return highscores;
     }
 }
